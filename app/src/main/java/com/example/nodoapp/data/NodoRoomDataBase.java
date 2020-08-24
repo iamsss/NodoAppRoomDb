c86c8184ed8e331a9472f74242c850c1be491034
iamsss <saurav9760@gmail.com>
2020-08-25 03:46:07 +0530

    Creating Nodo Room Database

---
+++ app/src/main/java/com/example/nodoapp/data/NodoRoomDataBase.java
@@ -0,0 +1,31 @@
+package com.example.nodoapp.data;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.example.nodoapp.model.Nodo;
+
+@Database(entities = {Nodo.class},version = 1)
+public abstract class NodoRoomDataBase extends RoomDatabase {
+    private static volatile NodoRoomDataBase INSTANCE;
+
+    public abstract NodoDao nodoDao();
+
+    static NodoRoomDataBase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (NodoRoomDataBase.class) {
+                if (INSTANCE == null) {
+                    // Create our db
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            NodoRoomDataBase.class, "nodo_database")
+                            .build();
+                }
+            }
+        }
+        return  INSTANCE;
+    }
+
+}
