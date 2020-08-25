61f1d6acb666bfae82b499e2087c62e47436821c
iamsss <saurav9760@gmail.com>
2020-08-25 06:05:01 +0530

    Adding New Nodo Activity

---
+++ app/src/main/java/com/example/nodoapp/NewNodoActivity.java
@@ -0,0 +1,43 @@
+package com.example.nodoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class NewNodoActivity extends AppCompatActivity {
+
+    private TextView editText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_nodo);
+
+        editText = findViewById(R.id.edit_txt);
+        final Button btn = findViewById(R.id.save_btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent replyIntent = new Intent();
+                if(TextUtils.isEmpty(editText.getText())){
+                    setResult(RESULT_CANCELED,replyIntent);
+                }else{
+                    String nodoStr = editText.getText().toString();
+                    replyIntent.putExtra("MyReply",nodoStr);
+                    setResult(RESULT_OK,replyIntent);
+                }
+                finish();
+            }
+        });
+
+    }
+}
